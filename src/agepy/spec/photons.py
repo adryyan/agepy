e400eb192d85e60eedf4c6797b37b0b36414ba12
Adrian Peter Krone <adrian.krone@physik.uni-kassel.de>
2025-01-17 16:10:54 +0100

    Clean up

--- src/agepy/spec/photons.py
+++ src/agepy/spec/photons.py
@@ -22,17 +22,15 @@ class Spectrum:
         raw: np.ndarray,
         time: int = None,
         anode: PositionAnode = None,
-        target_density: float = None,
-        intensity_downstream: float = None,
-        intensity_upstream: float = None,
+        **norm,
     ) -> None:
         # Store the passed data
         self._raw = raw
         self._t = time
         self.anode = anode
-        self._target_density = target_density
-        self._intensity_downstream = intensity_downstream
-        self._intensity_upstream = intensity_upstream
+        self._norm = list(norm.keys())
+        for key, value in norm.items():
+            setattr(self, key, value)
 
     @classmethod
     def from_h5(cls,
@@ -41,86 +39,29 @@ class Spectrum:
         time: int = None,
         anode: PositionAnode = None,
         target_density: str = None,
-        intensity_downstream: str = None,
         intensity_upstream: str = None,
+        **norm,
     ) -> None:
         with h5py.File(file_path, "r") as h5:
             # Load the raw data
             raw = np.asarray(h5[raw])
-            # Load the target density
+            # Add deprecated normalization parameters for backwards compatibility
             if target_density is not None:
-                target_density = h5[target_density][0]
-                print("Target density:", target_density)
-            # Load the upstream intensity
+                norm["target_density"] = target_density
             if intensity_upstream is not None:
-                intensity_upstream = h5[intensity_upstream][0]
-            # Load the downstream intensity
-            if intensity_downstream is not None:
-                intensity_downstream = h5[intensity_downstream][0]
+                norm["intensity_upstream"] = intensity_upstream
+            # Load normalization values
+            for key, h5path in norm.items():
+                if h5path not in h5:
+                    raise ValueError("Normalization parameter not found.")
+                norm[key] = h5[h5path][0]
         # Initialize the Spectrum
-        return cls(raw, time=time, anode=anode, target_density=target_density,
-                   intensity_downstream=intensity_downstream,
-                   intensity_upstream=intensity_upstream)
-
-    @classmethod
-    def from_scan(cls,
-        file_path: str,
-        scan_var: str = None,
-        raw: str = "dld_rd#raw/0",
-        time_per_step: int = None,
-        anode: PositionAnode = None,
-        target_density: str = None,
-        intensity_downstream: str = None,
-        intensity_upstream: str = None,
-    ) -> Tuple[list, list]:
-        with h5py.File(file_path, "r") as h5:
-            # Load the steps
-            if scan_var is not None:
-                steps = h5[scan_var]
-            # Load the raw data
-            raw = h5[raw]
-            # Load the target density
-            if target_density is not None:
-                target_density = np.asarray(h5[target_density])
-            else:
-                target_density = np.full(len(raw), None)
-            # Load upstream intensity
-            if intensity_upstream is not None:
-                intensity_upstream = np.asarray(h5[intensity_upstream])
-            else:
-                intensity_upstream = np.full(len(raw), None)
-            # Load downstream intensity
-            if intensity_downstream is not None:
-                intensity_downstream = np.asarray(h5[intensity_downstream])
-            else:
-                intensity_downstream = np.full(len(raw), None)
-            # Format the data and steps
-            spectra = []
-            step_val = []
-            for i, step in enumerate(raw.keys()):
-                # Format the step value
-                if scan_var is not None:
-                    step_val.append(steps[step][0][0])
-                else:
-                    step_val.append(float(step))
-                # Format the raw data
-                data = np.asarray(raw[step])
-                # Initialize the spectrum instance
-                spectra.append(cls(
-                    data,
-                    time=time_per_step,
-                    anode=anode,
-                    target_density=target_density[i],
-                    intensity_downstream=intensity_downstream[i],
-                    intensity_upstream=intensity_upstream[i]
-                ))
-        # Return the spectra and energies
-        return spectra, step_val
+        return cls(raw, time=time, anode=anode, **norm)
 
     def counts(self,
         anode: PositionAnode = None,
         roi: dict = None,
-        efficiency_map: Tuple[np.ndarray, np.ndarray, np.ndarray] = None,
+        qeff: Tuple[np.ndarray, np.ndarray] = None,
         background: Spectrum = None,
     ) -> Tuple[float, float]:
         """Get number of counts in the spectrum and the estimated
@@ -150,13 +91,11 @@ class Spectrum:
             det_image = det_image[det_image[:,0] > roi["x"]["min"]]
             det_image = det_image[det_image[:,0] < roi["x"]["max"]]
         # Apply spatial detector efficiency correction
-        if efficiency_map is not None:
-            eff, xe, ye = efficiency_map
+        if qeff is not None:
+            eff, xe = qeff
             x_inds = np.digitize(det_image[:,0], xe)
-            y_inds = np.digitize(det_image[:,1], ye)
-            efficiencies = eff[x_inds, y_inds]
             # Get the inverse of the efficiency
-            inv_eff = 1 / efficiencies[efficiencies > 0]
+            inv_eff = 1 / eff[x_inds]
         else:
             inv_eff = np.ones(det_image.shape[0])
         # Calculate the number of counts
@@ -168,28 +107,24 @@ class Spectrum:
             err /= self._t
         # Subtract background before further normalization
         if background is not None:
-            bkg_counts, bkg_err = background.counts(anode, roi=roi,
-                efficiency_map=efficiency_map)
+            bkg_counts, bkg_err = background.counts(anode, roi=roi, qeff=qeff)
             n -= bkg_counts
             n = max(n, 0)
             err = np.sqrt(err**2 + bkg_err**2)
         # Normalize data to account for beam intensity, gas
         # pressure, etc.
-        if self._target_density is not None:
-            n /= self._target_density
-            err /= self._target_density
-        if self._intensity_upstream is not None:
-            n /= self._intensity_upstream
-            err /= self._intensity_upstream
+        for value in self._norm:
+            n /= getattr(self, value)
+            err /= getattr(self, value)
         return n, err
 
     def spectrum(self,
         edges: np.ndarray,
         anode: PositionAnode = None,
         roi: dict = None,
-        efficiency_map: Tuple[np.ndarray, np.ndarray, np.ndarray] = None,
+        qeff: Tuple[np.ndarray, np.ndarray] = None,
         background: Spectrum = None,
-        phem_calib: Tuple[float, float] = None,
+        calib: Tuple[float, float] = None,
     ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Notes
@@ -210,19 +145,18 @@ class Spectrum:
         # Apply y roi filter
         det_image = det_image[det_image[:,1] > roi["y"]["min"]]
         det_image = det_image[det_image[:,1] < roi["y"]["max"]]
-        # Apply spatial detector efficiency correction
-        if efficiency_map is not None:
-            eff, xe, ye = efficiency_map
-            x_inds = np.digitize(det_image[:,0], xe)
-            y_inds = np.digitize(det_image[:,1], ye)
-            efficiencies = eff[x_inds, y_inds]
-        else:
-            efficiencies = np.ones(det_image.shape[0])
         # Don't need y values anymore
         det_image = det_image[:,0].flatten()
+        # Apply spatial detector efficiency correction
+        if qeff is not None:
+            eff, xe = qeff
+            x_inds = np.digitize(det_image, xe)
+            eff = 1 / eff[x_inds]
+        else:
+            eff = np.ones(det_image.shape[0])
         # Convert x values to wavelengths
-        if phem_calib is not None:
-            a, b = phem_calib
+        if calib is not None:
+            a, b = calib
             det_image = a * det_image + b
             # Adjust x roi filter to wavelength binning
             wl_min = edges[edges > (a * roi["x"]["min"] + b)][0]
@@ -235,11 +169,16 @@ class Spectrum:
             xroi = (det_image >= x_min) & (det_image <= x_max)
         # Apply x roi filter
         det_image = det_image[xroi]
-        efficiencies = efficiencies[xroi]
+        eff = eff[xroi]
         # Calculate the sum of weights for each bin, i.e. the weighted spectrum
-        spectrum = np.histogram(det_image, bins=edges, weights=efficiencies)[0]
+        spectrum = np.histogram(det_image, bins=edges, weights=eff)[0]
         # Histogram data without the per event efficiencies
-        errors = np.sqrt(np.histogram(det_image, bins=edges)[0])
+        hist = np.histogram(det_image, bins=edges)[0]
+        # Calculate the uncertainties
+        nonzero = hist > 0
+        per_bin_eff = np.ones_like(hist, dtype=float)
+        per_bin_eff[nonzero] = spectrum[nonzero] / hist[nonzero]
+        errors = np.sqrt(hist) * per_bin_eff
         # Normalize data to measurement duration per step
         if self._t is not None:
             spectrum /= self._t
@@ -247,20 +186,16 @@ class Spectrum:
         # Subtract background before further normalization
         if background is not None:
             bkg_spec, bkg_err = background.spectrum(
-                edges, anode, roi=roi, efficiency_map=efficiency_map,
-                phem_calib=phem_calib
+                edges, anode, roi=roi, qeff=qeff, calib=calib
             )
             spectrum -= bkg_spec
             spectrum[spectrum < 0] = 0
             errors = np.sqrt(errors**2 + bkg_err**2)
         # Normalize data to account for beam intensity, gas 
         # pressure, etc.
-        if self._target_density is not None:
-            spectrum /= self._target_density
-            errors /= self._target_density
-        if self._intensity_upstream is not None:
-            spectrum /= self._intensity_upstream
-            errors /= self._intensity_upstream
+        for value in self._norm:
+            spectrum /= getattr(self, value)
+            errors /= getattr(self, value)
         # Return the spectrum and uncertainties
         return spectrum, errors
 
@@ -313,20 +248,23 @@ class Scan:
         raw: str = "dld_rd#raw/0",
         time_per_step: int = None,
         target_density: str = None,
-        intensity_downstream: str = None,
         intensity_upstream: str = None,
+        **norm,
     ) -> None:
         self.anode = anode  
         self.spectra = []
         self.steps = []
         if isinstance(data_files, str):
             data_files = [data_files]
+        # Add deprecated normalization parameters for backwards compatibility
+        if target_density is not None:
+            norm["target_density"] = target_density
+        if intensity_upstream is not None:
+                norm["intensity_upstream"] = intensity_upstream
         for f in data_files:
-            spec, steps = Spectrum.from_scan(
+            spec, steps = self._load_spectra(
                 f, scan_var=scan_var, raw=raw, time_per_step=time_per_step,
-                anode=None, target_density=target_density,
-                intensity_downstream=intensity_downstream,
-                intensity_upstream=intensity_upstream
+                anode=None, **norm
             )
             self.spectra.extend(spec)
             self.steps.extend(steps)
@@ -337,6 +275,42 @@ class Scan:
         _sort = np.argsort(self.steps)
         self.steps = self.steps[_sort]
         self.spectra = self.spectra[_sort]
+
+    def _load_spectra(self,
+        file_path: str,
+        scan_var: str = None,
+        raw: str = "dld_rd#raw/0",
+        time_per_step: int = None,
+        anode: PositionAnode = None,
+        **norm,
+    ) -> Tuple[list, list]:
+        with h5py.File(file_path, "r") as h5:
+            # Load the steps
+            if scan_var is not None:
+                steps = h5[scan_var]
+            # Load the raw data
+            raw = h5[raw]
+            # Load normalization values
+            for key, h5path in norm.items():
+                norm[key] = np.asarray(h5[h5path])
+            # Format the data and steps
+            spectra = []
+            step_val = []
+            for i, step in enumerate(raw.keys()):
+                # Format the step value
+                if scan_var is not None:
+                    step_val.append(steps[step][0][0])
+                else:
+                    step_val.append(float(step))
+                # Format the raw data
+                data = np.asarray(raw[step])
+                # Format the normalization values
+                step_norm = {key: value[i] for key, value in norm.items()}
+                # Initialize the spectrum instance
+                spectra.append(Spectrum(data, time=time_per_step, anode=anode,
+                                        **step_norm))
+        # Return the spectra and energies
+        return spectra, step_val
 
     def counts(self,
         roi: dict = None,
@@ -435,12 +409,11 @@ class EnergyScan(Scan):
         raw: str = "dld_rd#raw/0",
         time_per_step: int = None,
         target_density: str = None,
-        intensity_downstream: str = None,
         intensity_upstream: str = None,
+        **norm,
     ) -> None:
         super().__init__(data_files, anode, energies, raw, time_per_step,
-                         target_density, intensity_downstream,
-                         intensity_upstream)
+                         target_density, intensity_upstream, **norm)
 
     @property
     def energies(self) -> np.ndarray:
